5f1f9fb5869467a1a644db8d3fa4eb0e989e2fde
Rafael Toledo <rafaeltoledo@MacBook-Pro-de-Rafael.local>
2020-12-20 19:21:56 -0300

    Compatibilidade com cordova-ios-6

---
+++ src/ios/CDVWebViewEngine+FileXhrFix.h
@@ -0,0 +1,13 @@
+//
+//  CDVWebViewEngine+FileXhrFix.h
+//  HelloCordova
+//
+//  Created by Connor Pearson on 2/9/17.
+//
+//
+
+#import "CDVWebViewEngine.h"
+
+@interface CDVWebViewEngine (FileXhrFix)
+
+@end
